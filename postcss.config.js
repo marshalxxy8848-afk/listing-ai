8095801ebde6a2860d683f0020900f5c1c88b548
Ubuntu <ubuntu@localhost.localdomain>
2026-05-11 01:05:26 +0800

    fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,7 @@
-const config = { plugins: { "@tailwindcss/postcss": {} } };
-export default config;
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+module.exports = config;
